821121edc457cbdc5f9d68b3a4f5faab24ec86ef
Petar <marincicpetar@gmail.com>
2024-09-16 18:42:57 +0200

    added pagination component

---
+++ resources/js/Components/Pagination.jsx
@@ -0,0 +1,25 @@
+import { Link } from "@inertiajs/react";
+
+function Pagination({ links }) {
+    return (
+        <nav className="text-center mt-4">
+            {links.map((link) => (
+                <Link
+                    preserveScroll
+                    href={link.url || ""}
+                    key={link.label}
+                    className={
+                        "inline-block py-2 px-3 rounded-lg text-gray-200 " +
+                        (link.active ? "bg-gray-500 " : " ") +
+                        (!link.url
+                            ? "!text-gray-950 cursor-not-allowed "
+                            : "hover:bg-gray-950")
+                    }
+                    dangerouslySetInnerHTML={{ __html: link.label }}
+                ></Link>
+            ))}
+        </nav>
+    );
+}
+
+export default Pagination;
