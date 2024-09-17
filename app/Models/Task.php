9e8913a4a6079809eedd03d7fc7811016b4c03f5
Petar <marincicpetar@gmail.com>
2024-09-17 12:06:11 +0200

    task - index and resources

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     use HasFactory;
+
+
+
+
+    public function assignedUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "assigned_user_id");
+    }
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "created_by");
+    }
+
+    public function updatedBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "updated_by");
+    }
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
